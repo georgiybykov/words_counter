65a18195dddaa00be6acd1d60bfcae2cdac69c13
Georgiy Bykov <zhora.bykov@gmail.com>
2021-12-26 19:54:09 +0300

    Add ex_doc to generate documentation

--- mix.exs
+++ mix.exs
@@ -34,7 +34,8 @@ defmodule WordsCounter.MixProject do
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:credo_contrib, "~> 0.2", only: [:dev, :test], runtime: false},
-      {:excoveralls, "~> 0.10", only: :test}
+      {:excoveralls, "~> 0.10", only: :test},
+      {:ex_doc, "~> 0.24", only: :dev, runtime: false}
     ]
   end
 end
